7ceb5c59e8b90d05a24827052a730137f8b1b786
Zhivko Milev <z.milev@draftkings.com>
2022-01-06 19:29:58 +0200

    Add sort solution

---
+++ DataStructuresSolutions/01.LinearDS/02SortWords.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.LinearDS
+{
+    public static class _02SortWords
+    {
+        public static void QuickSort(this string[] numbers)
+        {
+            QuickSortInternal(numbers, 0, numbers.Length - 1);
+        }
+
+        private static void QuickSortInternal(string[] numbers, int left, int right)
+        {
+            if (left < right)
+            {
+                var pivotIndex = Partition(numbers, left, right);
+                QuickSortInternal(numbers, left, pivotIndex - 1);
+                QuickSortInternal(numbers, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(string[] numbers, int left, int right)
+        {
+            int i = left;
+            string pivot = numbers[right];
+            string temp;
+
+            for (int j = left; j <= right; j++)
+            {
+                if (numbers[j] < pivot)
+                {
+                    temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = temp;
+                    i++;
+                }
+            }
+
+            temp = numbers[right];
+            numbers[right] = numbers[i];
+            numbers[i] = temp;
+            return i;
+        }
+    }
+}
--- DataStructuresSolutions/01.LinearDS/Program.cs
+++ DataStructuresSolutions/01.LinearDS/Program.cs
@@ -1,50 +1,7 @@
 ﻿var input = Console.ReadLine()
     ?.Split(' ', StringSplitOptions.RemoveEmptyEntries)
     .Select(num => int.Parse(num))
-    .ToList();
+    .ToArray()
+    .QuickSort();
 
-var sorted = Sort(input);
 Console.WriteLine(string.Join(", ", sorted));
-
-void SumAndAverage(string[]? input)
-{
-    if (input == null || input.Length == 0)
-    {
-        Console.WriteLine("Sum=0; Average=0.00");
-        return;
-    }
-
-    double sum = 0;
-    for (int i = 0; i < input!.Length; i++)
-    {
-        sum += int.Parse(input[i]);
-    }
-
-    var average = sum / input.Length;
-    Console.WriteLine($"Sum={sum}; Average={average:0.00}");
-}
-
-List<int> Sort(List<int>? input)
-{
-    if (input.Count == 0) return new List<int>();
-    
-    int pivot = input[0];
-    List<int> leftThanPivot = new List<int>();
-    List<int> rightThanPivot = new List<int>();
-    for (int i = 1; i < input.Count; i++)
-    {
-        if (input[i] <= pivot)
-        {
-            leftThanPivot.Add(input[i]);
-        }
-        else
-        {
-            rightThanPivot.Add(input[i]);
-        }
-    }
-
-    var leftSorted = Sort(leftThanPivot);
-    var rightSorted = Sort(rightThanPivot);
-    leftSorted.AddRange(rightSorted);
-    return leftSorted;
-}
